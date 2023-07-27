5eef9a8f9608510cd9214d969230397580e2eb53
ankush170 <63419189+ankush170@users.noreply.github.com>
2023-07-27 17:51:02 +0530

    delete button(front end)

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -141,7 +141,7 @@ function TweetCard({
           likedByMe={likedByMe}
           likeCount={likeCount}
         />
-        <DelButton 
+        <DeleteButton 
         className="self-end"/>
       </div>
     </li>
@@ -195,31 +195,30 @@ function HeartButton({
       <span>{likeCount}</span>
     </button>
   );
+}
 
+type delButtonProps = {
+  small?: boolean;
+  gray?: boolean;
+  className?: string;
+} & DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
 
-  type delButtonProps = {
-    small?: boolean;
-    gray?: boolean;
-    className?: string;
-  } & DetailedHTMLProps<
-    ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  >;
+function DeleteButton({
+  small = false,
+  gray = false,
+  className = "",
+  ...props
+}: delButtonProps) {
+  const sizeClasses = small ? "px-2 py-1" : "px-4 py-2 font-bold";
+  const colorClasses = gray ? "bg-gray-400 hover:bg-gray-300 focus-visible:bg-gray-300" : "bg-red-500 hover:bg-red-400 focus-visible:bg--400";
 
-  function DelButton({
-    small = false,
-    gray = false,
-    className = "",
-    ...props
-  }: delButtonProps) {
-    const sizeClasses = small ? "px-2 py-1" : "px-4 py-2 font-bold";
-    const colorClasses = gray ? "bg-gray-400 hover:bg-gray-300 focus-visible:bg-gray-300" : "bg-red-500 hover:bg-red-400 focus-visible:bg--400";
-  
-    return (
-      <button
-        className={`rounded-full text-white transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 ${sizeClasses} ${colorClasses} ${className}`}
-        {...props}
-      >Delete</button>
-    );
-  }
+  return (
+    <button
+      className={`rounded-full text-white transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 ${sizeClasses} ${colorClasses} ${className}`}
+      {...props}
+    >Delete</button>
+  );
 }
